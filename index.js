09e7f828650821c2132ead9bdae5d626b4b5f41a
hemantjnypc <44284191+hemantjnypc@users.noreply.github.com>
2019-03-03 16:02:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //js//
 
-  
 var GridLayout = VueGridLayout.GridLayout;
 var GridItem = VueGridLayout.GridItem;
 var app = new Vue({
@@ -14,83 +13,93 @@ var app = new Vue({
       layout: [
       {
           "x": 0,
-          "y": 3,
-          "w": 5,
-          "h": 3,
-          "i": "0",
+          "y": 6,
+          "w": 2,
+          "h": 2,
+          "i": "1",
+          "label":"",
           "content": "<b>DESIGN THINKING - AN IMPERATIVE</b>"
         },
          {
-          "x": 5,
-          "y": 6,
+          "x": 4,
+          "y": 22,
           "w": 3,
-          "h": 7,
-          "i": "1",
+          "h": 5,
+          "i": "2",
+          "label":"",
           "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/ZJ6yKEK.png'  />"
         },
         {
-          "x": 5,
+          "x": 4,
           "y": 0,
-          "w": 3,
+          "w": 4,
           "h": 6,
-          "i": "2",
+          "i": "3",
+          "label":"Real-time Inputs",
           "content": "<iframe width='450' height='260' style='border: 1px solid #cccccc;' src='https://thingspeak.com/channels/692115/charts/1?bgcolor=%23d9ff46&color=%23112b00&days=1&dynamic=true&results=10&type=line'></iframe>"
         },
       {
           "x": 0,
           "y": 0,
-          "w": 3,
-          "h": 3,
-          "i": "3",
+          "w": 2,
+          "h": 6,
+          "i": "4",
+          "label":"Why Design Thinking",
           "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/rmhvzo8.png'  />"
         },
         {
-          "x": 3,
+          "x": 2,
           "y": 0,
           "w": 2,
-          "h": 3,
-          "i": "4",
+          "h": 8,
+          "i": "5",
+          "label":"Human Centered",
           "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/cdVmgrJ.png'  />"
         },
         {
-          "x": 0,
-          "y": 6,
-          "w": 3,
-          "h": 7,
-          "i": "5",
+          "x": 4,
+          "y": 12,
+          "w": 4,
+          "h": 10,
+          "i": "6",
+          "label":"Steps",
           "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/L7jRRzs.png'  />"
           },
         {
-          "x": 3,
-          "y": 10,
-          "w": 2,
-          "h": 3,
-          "i": "6",
-          "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/W6uNymb.png'  />"
+          "x": 0,
+          "y": 20,
+          "w": 4,
+          "h": 7,
+          "i": "7",
+          "label":"DT in STEM",
+          "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/FTKNHLX.png'  />"
         },
         {
-          "x": 3,
+          "x": 4,
           "y": 6,
-          "w": 2,
-          "h": 4,
-          "i": "7",
+          "w": 4,
+          "h": 6,
+          "i": "8",
+          "label":"A Well-Known Example",
           "content": "<img style='width:80%;height:80%;object-fit:cover;' src='https://i.imgur.com/8nMAfy3.png'  />"
         },
         {
           "x": 0,
-          "y": 13,
-          "w": 4,
-          "h": 6,
-          "i": "8",
-          "content": "<iframe style='width:80%;height:80%;object-fit:cover;' src='https://www.youtube.com/embed/_r0VX-aU_T8'></iframe>"
-        },
-        {
-          "x": 4,
-          "y": 13,
+          "y": 14,
           "w": 4,
           "h": 6,
           "i": "9",
-          "content": "image slider"
+          "label":"Process",
+          "content": "<iframe style='width:80%;height:80%;object-fit:cover;' src='https://www.youtube.com/embed/_r0VX-aU_T8'></iframe>"
+        },
+        {
+          "x": 0,
+          "y": 8,
+          "w": 4,
+          "h": 6,
+          "i": "10",
+          "label":"",
+          "content": "..Image Slider.."
         }        
         ],
     }},
